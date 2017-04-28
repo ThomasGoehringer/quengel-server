a006f22b9997c4bae6d9706dbe3c263192258742
Unknown <t.goehringer1995@web.de>
2017-04-28 17:39:35 +0200

    Added basic profile schema (no field for image)

---
+++ src/models/profile.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const profileSchema = new Schema({
+  name: String,
+  birthday: Date,
+  entries: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Entry'
+  }],
+  createdAt: Date,
+  updatedAt: Date
+});
+
+profileSchema.pre('save', function (next) {
+  const currentDate = new Date();
+  this.updatedAt = currentDate;
+  if (!this.createdAt) {
+    this.createdAt = currentDate;
+  }
+  next();
+});
+
+const Profile = mongoose.model('Entry', profileSchema);
+
+module.exports = Profile;
